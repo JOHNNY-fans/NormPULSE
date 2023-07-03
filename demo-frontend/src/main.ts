0a919434b6ca7e5978401619ac47170462836076
JOHNNY-fans <47244713+JOHNNY-fans@users.noreply.github.com>
2023-07-03 13:25:19 +0800

    Update main.ts

--- demo-frontend/src/main.ts
+++ demo-frontend/src/main.ts
@@ -23,8 +23,4 @@ app.component("vChart", ECharts);
 app.mount("#app");
 
 export const BASE_API =
-  // "https://dsw-gateway-cn-beijing.data.aliyun.com/dsw-27313/ide/proxy/2233/"
-  // "api"
   "http://localhost:2233/";
-// "https://58.34.83.130:0/"
-// "http://10.246.19.186:2233/"
